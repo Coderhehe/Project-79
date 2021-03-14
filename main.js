2d3f43df04a4b5cd2453569085ad65378f4e5b42
Coderhehe <77240747+Coderhehe@users.noreply.github.com>
2021-03-14 14:42:53 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,20 +1,30 @@
 pmarray = [
-    "Chicken Pizza",
-    "Cheese Pizza",
-    "Vegetable Pizza",
-    "Pepporini Pizza",
-    "Pineapple Pizza",
-    "Alferdo Pasta"
+    "Chicken Pizza","Cheese Pizza","Vegetable Pizza","Pepporini Pizza","Pineapple Pizza","Indian Pizza"
 ];
 function showmenup() {
   var htmldata;
   htmldata= "<ol class='menulist'>"
   pmarray.sort();
-for ( var i = 0;   pmarray.length; i++) {
-    htmldata=htmldata+"<li>"+ pmarray[i] + "<li>"
+for ( var i = 0; i < pmarray.length; i++) {
+    htmldata=htmldata+"<li>"+ pmarray[i] + "</li>"
     
 }
-htmldata=htmldata+"<ol>" 
+htmldata=htmldata+"</ol>" 
 document.getElementById("display_menu").innerHTML = htmldata;
 }
+
+function add_item() {
+    var htmldata;
+    var item= document.getElementById("add_item").value;
+    pmarray.push(item);
+    pmarray.sort();
+    htmldata= "<section class='cards'>"
+    for ( var i = 0; i < pmarray.length; i++) {
+        htmldata=htmldata+"<div class='cards'>"+ "<img src='images/pizzaImg.png'>"+pmarray[i] + "</div>"
+        
+    }
+    htmldata=htmldata+"</section>" 
+    document.getElementById("display_addedmenu").innerHTML = htmldata;    
+        
+    }
 
